45b0ac0dbea60a6e51e9c70ac81fa2dd6d79ce1f
Niall Grant <ngfgrant@gmail.com>
2019-07-02 11:06:21 +0100

    Add python setup files

---
+++ setup.py
@@ -0,0 +1,66 @@
+from setuptools import setup, find_packages
+
+__version__ = "0.1.0"
+
+
+RESOLVER_NAME = 'resolver'  # lowercase, snakecase
+RESOLVER_MODULE_NAME = 'resolver.resolver'  # name of resolver module e.g. resolver.py
+RESOLVER_COMMAND_NAME = 'custom_resolver'  # how to call the resolver in sceptre e.g. !command_name
+RESOLVER_CLASS = 'CustomResolver'  # CamelCase name of resolver class
+RESOLVER_DESCRIPTION = ''  # one line summary description
+RESOLVER_AUTHOR = 'Sceptre'  # if multiple use a single string with comma separated names.
+RESOLVER_AUTHOR_EMAIL = 'sceptre@cloudreach.com'  # if multiple use single string with commas.
+RESOLVER_URL = 'https://github.com/sceptre/'  # GitHub repo of your resolver
+
+with open("README.md") as readme_file:
+    README = readme_file.read()
+
+install_requirements = [
+    "packaging==16.8",
+]
+
+test_requirements = [
+    "pytest>=3.2",
+]
+
+setup_requirements = [
+    "pytest-runner>=3"
+]
+
+setup(
+    name=RESOLVER_NAME,
+    version=__version__,
+    description=RESOLVER_DESCRIPTION,
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author=RESOLVER_AUTHOR,
+    author_email=RESOLVER_AUTHOR_EMAIL,
+    license='Apache2',
+    url=RESOLVER_URL,
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    py_modules=[RESOLVER_MODULE_NAME],
+    entry_points={
+        'sceptre.resolvers': [
+            "{}={}:{}".format(RESOLVER_COMMAND_NAME, RESOLVER_MODULE_NAME, RESOLVER_CLASS)
+        ]
+    },
+    include_package_data=True,
+    zip_safe=False,
+    keywords="sceptre, sceptre-resolver",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Environment :: Console",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
+    ],
+    test_suite="tests",
+    install_requires=install_requirements,
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+    extras_require={
+        "test": test_requirements
+    }
+)
