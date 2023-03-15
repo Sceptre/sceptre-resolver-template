6a4f4e47b88294c0913c2ce87e330d69732cb06d
Khai Do <zaro0508@gmail.com>
2023-03-15 10:59:38 -0700

    fix linter error

--- resolver/resolver.py
+++ resolver/resolver.py
@@ -2,7 +2,6 @@ from sceptre.resolvers import Resolver
 
 
 class CustomResolver(Resolver):
-
     def resolve(self):
         """
         resolve is the method called by Sceptre. It should carry out the work
