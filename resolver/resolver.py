eca93fbd9db45294c2bcd7a7d53aa7f07a6c6c77
Khai Do <zaro0508@gmail.com>
2023-03-14 17:23:43 -0700

    remove uneeded resolver init

--- resolver/resolver.py
+++ resolver/resolver.py
@@ -2,8 +2,6 @@ from sceptre.resolvers import Resolver
 
 
 class CustomResolver(Resolver):
-    def __init__(self, *args, **kwargs):
-        super(CustomResolver, self).__init__(*args, **kwargs)
 
     def resolve(self):
         """
